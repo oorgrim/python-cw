8e89c9c1a8747475e36d43aac8cf1b3ddd76f249
oorgrim <oorgrim@gmail.com>
2023-11-16 21:49:14 +0600

    типы данных переменные практика 16.10

---
+++ practise1types.py
@@ -0,0 +1,68 @@
+# practice
+
+# user_input = input("Добрый день! Ввеите что-нибудь: ")
+ 
+# print("Вы ввели", user_input)
+
+
+# 1
+
+# user_input = input("Введите число: ")
+# number = int(user_input)
+# result = number ** 2
+# print("Ваше число в кварате = ", result)
+
+
+# 2
+
+# user_input1 = input("Введите первое число: ")
+# user_input2 = input("Введите второе число: ") 
+# num1, num2 = float(user_input1) , float(user_input2)
+# average = (num1 + num2)/2
+# print("Среднее арифметическое ваших чисел равно = ", average)
+
+
+# 3
+
+# user_input = input("Введите длину стороны квадрата: ")
+# length = int(user_input)
+# square = length**2
+# print("Площадь квалрата заданной вами стороной = ", square)
+
+
+# 4
+
+# user_input = input("Ввелите километры: ")
+# km = float(user_input)
+# km_to_miles = km * 0.621371
+# print(km, "км в милях = ", km_to_miles, "миль")
+
+# 5
+
+# user_input1 = input("Введие первое число: ")
+# user_input2 = input("Введие первое число: ")
+
+# num1, num2 = float(user_input1), float(user_input2)
+# sum = num1 + num2
+# difference1 = num1 - num2
+# difference2 = num2 - num1
+# multiplication = num1 * num2
+# devision1 = num1 / num2
+# devision2 = num2 / num1
+# print("сумма ваших чисел = ", sum, ", разность первого числа и второго =  ", difference1, "разность второго числа и первого ", difference2, ", умножение = ", multiplication, "деление первого числа на второе = ", devision1, " и второго числа на первое = ", devision2)
+
+
+# 6
+
+# user_input1 = input("Введите а: ")
+# user_input2 = input("Введите b: ")
+
+# a, b = int(user_input1), int(user_input2)
+# x = 
+# example  = a *x + b
+
+# 7 
+
+user_input1 = input("введите часы: ")
+user_input2 = input("введите минуты: ")
+hours_now, minutes_now = int(user_input1), int(user_input2)
